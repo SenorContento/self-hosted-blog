5f26dab43b055179b4dc88c8a714fe9ab2989238
Alex Contento <github@senorcontento.com>
2019-01-10 21:22:56 -0500

    Added Basic UPS Detection Script

---
+++ apc/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>APC UPS Battery Stats</title>
+    <link rel="stylesheet" href="/HTML-CSS/assignment1.css">
+    <link rel="icon" href="images/svg/SenorContento.svg">
+    <link rel="icon" href="images/png/SenorContento-1024x1024.png">
+  </head>
+  <body>
+    <code>
+      <?php system('/sbin/apcaccess');;?>
+    </code>
+  </body>
+</html>
