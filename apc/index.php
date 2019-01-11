e6d776501f92fc8fc45e7c9e6a883d355dd054a4
Alex Contento <github@senorcontento.com>
2019-01-10 21:40:26 -0500

    Changing some looks of the apc page

--- apc/index.php
+++ apc/index.php
@@ -3,12 +3,15 @@
   <head>
     <title>APC UPS Battery Stats</title>
     <link rel="stylesheet" href="/HTML-CSS/assignment1.css">
-    <link rel="icon" href="images/svg/SenorContento.svg">
-    <link rel="icon" href="images/png/SenorContento-1024x1024.png">
+    <link rel="icon" href="/images/svg/SenorContento.svg">
+    <link rel="icon" href="/images/png/SenorContento-1024x1024.png">
   </head>
   <body>
-    <code>
-      <?php system('/sbin/apcaccess');;?>
-    </code>
+    <pre>
+      <!--If only inheritance was a part of the css standard-->
+      <code style="color: #90EE90">
+        <?php system('/sbin/apcaccess');;?>
+      </code>
+    </pre>
   </body>
 </html>
