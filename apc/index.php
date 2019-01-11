8c1f1eab8f7af0f5c0f9a55e6d26824be35f3e72
Alex Contento <github@senorcontento.com>
2019-01-10 21:48:21 -0500

    Added Extra Line

--- apc/index.php
+++ apc/index.php
@@ -10,7 +10,7 @@
       <!--If only inheritance was a part of the css standard-->
       <pre><code style="color: #90EE90"><?php system('/sbin/apcaccess');
 
-      <br>
+      print('</br>');
       system('/bin/date -u'); ?></code></pre>
   </body>
 </html>
