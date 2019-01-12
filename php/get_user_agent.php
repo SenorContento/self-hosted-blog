058a4766b33e7d4c5812530a047083fb17feb90b
Alex Contento <github@senorcontento.com>
2019-01-11 19:23:46 -0500

    Added User Agent Script

---
+++ php/get_user_agent.php
@@ -0,0 +1,15 @@
+<?php $PageTitle="Get User Agent Test Page";
+
+  function customPageHeader(){
+    print('<!--This Comment is Here Just to Mark Custom Header Inclusion in PHP!!!-->');
+  }
+
+  include_once($_SERVER['DOCUMENT_ROOT'] . "/php_data/header.php");
+
+  print("<p>Your User Agent: \"" . $_SERVER['HTTP_USER_AGENT'] . "\"</br></br>");
+
+  print("Other User Agents: </br>");
+  system("/usr/bin/awk -F'\"' '/GET/ {print $6}' /var/log/nginx/access.log | /usr/bin/cut -d' ' -f1 | /usr/bin/sort | /usr/bin/uniq -c | /usr/bin/sort -rn");
+  print("</p>");
+
+  include_once($_SERVER['DOCUMENT_ROOT'] . "/php_data/footer.php"); ?>
