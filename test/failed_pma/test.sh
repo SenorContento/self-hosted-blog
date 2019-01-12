4f642594df696b6a9ee62b5039b400f0a6efacd9
Alex Contento <github@senorcontento.com>
2019-01-12 03:35:43 -0500

    Test

--- test/failed_pma/test.sh
+++ test/failed_pma/test.sh
@@ -1,9 +1,9 @@
-/bin/zcat -f /var/log/nginx/access.log* | /bin/grep "pma_username" | /usr/bin/awk -F"pma_username=" '{print $2}' | /usr/bin/cut -d'&' -f1,2 | /usr/bin/cut -d' ' -f1 | /usr/bin/awk -F"&pma_password=" '{print $1 "," $2}'
+#/bin/zcat -f /var/log/nginx/access.log* | /bin/grep "pma_username" | /usr/bin/awk -F"pma_username=" '{print $2}' | /usr/bin/cut -d'&' -f1,2 | /usr/bin/cut -d' ' -f1 | /usr/bin/awk -F"&pma_password=" '{print $1 "\t" $2}'
 
-#count=1
-#while [ $count -lt 2000 ]
-#do
-#  filename="node${count}.shtml"
-#  echo "$count"
-#  count=`expr $count + 1`
-#done
+count=1
+while [ $count -lt 10000 ]
+do
+  filename="node${count}.shtml"
+  echo "$count"
+  count=`expr $count + 1`
+done
