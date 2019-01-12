c95b7bdcb56b72b7a0cbc4408e12cc165f1e4577
Alex Contento <github@senorcontento.com>
2019-01-11 19:31:52 -0500

    Changing User Agent Page

--- test/get_user_agent.php
+++ test/get_user_agent.php
@@ -8,8 +8,8 @@
 
   print("<p>Your User Agent: \"" . $_SERVER['HTTP_USER_AGENT'] . "\"</p></br>");
 
-  print("Other User Agents: </br><pre><code>");
+  print("<p>Other User Agents: </br><pre><code>");
   system("/usr/bin/awk -F'\"' '/GET/ {print $6}' /var/log/nginx/access.log | /usr/bin/cut -d' ' -f1 | /usr/bin/sort | /usr/bin/uniq -c | /usr/bin/sort -rn");
-  print("</code></pre>");
+  print("</code></pre></p>");
 
   include_once($_SERVER['DOCUMENT_ROOT'] . "/php_data/footer.php"); ?>
