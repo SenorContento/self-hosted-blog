7165ca1f687d2c7a3423bc8823257a09e4960ac9
Alex Contento <github@senorcontento.com>
2019-01-12 02:19:18 -0500

    Fixing Counter

--- test/failed_pma/index.php
+++ test/failed_pma/index.php
@@ -10,14 +10,18 @@
 
 <p>Failed Attempts at Hacking PHPMyAdmin:</p>
 <pre><code>
-User  Password
---------------
 <?php
 $failed_attempts = shell_exec("zcat -f /var/log/nginx/access.log* | /bin/grep \"phpmyadmin/index.php?pma_username\" | /usr/bin/cut -d'?' -f2 | /usr/bin/cut -d'&' -f1,2 | /usr/bin/cut -d'=' -f2- | /usr/bin/awk -F\"&pma_password=\" '{print $1 \"\\t\" $2}'");
 $failcount = shell_exec("/bin/echo \"" . $failed_attempts . "\" | wc -l");
 $uniq_failcount = shell_exec("/bin/echo \"" . $failed_attempts . "\" | sort | uniq | wc -l");
 
-print($failed_attempts . "<br><br>" . $failcount . " " . $uniq_failcount)
+print("Unique Fails: " . $uniq_failcount);
+print("Total Fails: " . $failcount);
+
+print("
+User  Password
+--------------
+" . $failed_attempts);
 ?>
 </code><pre>
 
