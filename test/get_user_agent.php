d745354d8879e986ccdb18bca8dbab54bdd5a7c2
Alex Contento <github@senorcontento.com>
2019-01-12 20:34:50 -0500

    Updating User Agent to New Command

--- test/get_user_agent.php
+++ test/get_user_agent.php
@@ -47,7 +47,7 @@ class userAgent {
     print("<p>All User Agents (Short): </br>");
     print("<pre><code>");
 
-    system($this->exec_find_path . " \"" . $this->log_path . "\" -name \"access.log*\" -follow -type f -print0 | " . $this->exec_xargs_path . " -0 " . $this->exec_gunzip_path . " -cf | " . $this->exec_cut_path . " -d' ' -f1 | " . $this->exec_sort_path . " | " . $this->exec_uniq_path . " -c | " . $this->exec_sort_path . " -rn");
+    system($this->exec_find_path . " \"" . $this->log_path . "\" -name \"access.log*\" -follow -type f -print0 | " . $this->exec_xargs_path . " -0 " . $this->exec_gunzip_path . " -cf | " . $this->exec_awk_path . " -F'\"' '/GET/ {print $6}' | " . $this->exec_cut_path . " -d' ' -f1 | " . $this->exec_sort_path . " | " . $this->exec_uniq_path . " -c | " . $this->exec_sort_path . " -rn");
     //system($this->exec_awk_path . " -F'\"' '/GET/ {print $6}' /var/log/nginx/access.log | /usr/bin/cut -d' ' -f1 | /usr/bin/sort | /usr/bin/uniq -c | /usr/bin/sort -rn");
 
     print("</code></pre>" . '\n' . "</p>");
