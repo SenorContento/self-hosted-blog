9e88b106b9d3398f16055b675d4aeeed1a3531f3
Alex Contento <github@senorcontento.com>
2019-01-13 11:39:55 -0500

    Verifying Shell Code not Injectable

--- test/failed_pma/index.php
+++ test/failed_pma/index.php
@@ -26,7 +26,7 @@ class loadPage {
 
 class PHPMyAdmin_Fails {
   # The below variables are for the production server
-  public $log_path = "/var/log/nginx/";
+  public $log_path = "/var/log/nginx";
   public $exec_find_path = "/usr/bin/find";
   public $exec_xargs_path = "/usr/bin/xargs";
   public $exec_gunzip_path = "/bin/gunzip";
@@ -40,7 +40,7 @@ class PHPMyAdmin_Fails {
   public $exec_sed_path = "/bin/sed";
 
   # The below variables are for testing on localhost
-  /*public $log_path = "/Users/senor/Documents/Class/2019/Spring/CSCI 3000/Nginx Server Logs/01:12:19/gzip_logs/";
+  /*public $log_path = "/Users/senor/Documents/Class/2019/Spring/CSCI 3000/Nginx Server Logs/01:12:19/gzip_logs";
   public $exec_find_path = "find";
   public $exec_xargs_path = "xargs";
   public $exec_gunzip_path = "gunzip";
@@ -72,20 +72,31 @@ class PHPMyAdmin_Fails {
 
   public function downloadCSV($failed_attempts) {
     $this->downloadHeaders("PHPMyAdmin_Fails_" . date("m-d-Y") . ".csv");
-    $failed_attempts_parsed = shell_exec($this->exec_echo_path . " " . escapeshellarg($failed_attempts) . " | " . $this->exec_awk_path . " -F\"&pma_password=\" -v x=\"\\\"\" '{print \",\\=\"x$1x\",\\=\"x$2x\"\"}'");
+    $failed_attempts_parsed = shell_exec($this->exec_echo_path . " " . escapeshellarg($failed_attempts) . " | " .
+    $this->exec_awk_path . " -F\"&pma_password=\" -v x=\"\\\"\" '{print \",\\=\"x$1x\",\\=\"x$2x\"\"}'");
 
     print(",User,Password");
-    system($this->exec_echo_path . " " . escapeshellarg($failed_attempts_parsed) . " | " . $this->exec_sort_path . " | " . $this->exec_uniq_path . " | " . $this->exec_grep_path . " -v \",=\\\"\\\",=\\\"\\\"\"" . " | " . $this->exec_sed_path . " 's/=\"\"//g'"); //,="",=""
+    system($this->exec_echo_path . " " . escapeshellarg($failed_attempts_parsed) . " | " .
+    $this->exec_sort_path . " | " .
+    $this->exec_uniq_path . " | " .
+    $this->exec_grep_path . " -v \",=\\\"\\\",=\\\"\\\"\"" . " | " .
+    $this->exec_sed_path . " 's/=\"\"//g'"); //,="",=""
 
     die();
   }
 
   public function printTable() {
     $failed_attempts = $this->generateTable();
-    $failed_attempts_parsed = shell_exec($this->exec_echo_path . " " . escapeshellarg($failed_attempts) . " | " . $this->exec_awk_path . " -F\"&pma_password=\" '{print \"<tr><td>\"$1\"</td><td>\"$2\"</td></tr>\"}'");
+    $failed_attempts_parsed = shell_exec($this->exec_echo_path . " " . escapeshellarg($failed_attempts) . " | " .
+    $this->exec_awk_path . " -F\"&pma_password=\" '{print \"<tr><td>\"$1\"</td><td>\"$2\"</td></tr>\"}'");
 
-    $failcount = shell_exec($this->exec_echo_path . " " . escapeshellarg($failed_attempts_parsed) . " | " . $this->exec_wc_path . " -l");
-    $uniq_failcount = shell_exec($this->exec_echo_path . " " . escapeshellarg($failed_attempts_parsed) . " | " . $this->exec_sort_path . " | " . $this->exec_uniq_path . " | " . $this->exec_wc_path . " -l");
+    $failcount = shell_exec($this->exec_echo_path . " " . escapeshellarg($failed_attempts_parsed) . " | " .
+    $this->exec_wc_path . " -l");
+
+    $uniq_failcount = shell_exec($this->exec_echo_path . " " . escapeshellarg($failed_attempts_parsed) . " | " .
+    $this->exec_sort_path . " | " .
+    $this->exec_uniq_path . " | " .
+    $this->exec_wc_path . " -l");
 
     print("<p>No Real Logins Show Up Here because Real Logins use POST requests, these attacks use GET requests.");
     print("</br></br><a href=\"?download_csv\">Download Table as CSV</a></p>");
@@ -100,12 +111,22 @@ class PHPMyAdmin_Fails {
     print("<br><br><table>");
     print("<tr><th>User</th><th>Password</tr>");
 
-    system($this->exec_echo_path . " " . escapeshellarg($failed_attempts_parsed) . " | " . $this->exec_sort_path . " | " . $this->exec_uniq_path . " | " . $this->exec_grep_path . " -v \"<tr><td></td><td></td></tr>\"");
+    system($this->exec_echo_path . " " . escapeshellarg($failed_attempts_parsed) . " | " .
+    $this->exec_sort_path . " | " .
+    $this->exec_uniq_path . " | " .
+    $this->exec_grep_path . " -v \"<tr><td></td><td></td></tr>\"");
+
     print("</table>");
   }
 
   public function generateTable() {
-    $failed_attempts = shell_exec($this->exec_find_path . " \"" . $this->log_path . "\" -name \"access.log*\" -follow -type f -print0 | " . $this->exec_xargs_path . " -0 " . $this->exec_gunzip_path . " -cf | " . $this->exec_grep_path . " \"pma_username\" | " . $this->exec_awk_path . " -F\"pma_username=\" '{print $2}' | " . $this->exec_cut_path . " -d'&' -f1,2 | " . $this->exec_cut_path . " -d' ' -f1");
+    $failed_attempts = shell_exec($this->exec_find_path . " \"" . // Injection Free
+    $this->log_path . "\" -name \"access.log*\" -follow -type f -print0 | " . // Injection Free
+    $this->exec_xargs_path . " -0 " . $this->exec_gunzip_path . " -cf | " . // Injection Free
+    $this->exec_grep_path . " \"pma_username\" | " . // Appears to Be Good
+    $this->exec_awk_path . " -F\"pma_username=\" '{print $2}' | " . // Appears to Be Good
+    $this->exec_cut_path . " -d'&' -f1,2 | " . // Appears to Be Good
+    $this->exec_cut_path . " -d' ' -f1"); // Appears to Be Good
 
     if(isset($_GET['download_csv'])) $this->downloadCSV($failed_attempts);
 
