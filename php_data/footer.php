80ebc33de2ee4715e01f0b12bf246d8ed36926c9
Alex Contento <github@senorcontento.com>
2019-01-18 20:57:46 -0500

    Added footer.

--- php_data/footer.php
+++ php_data/footer.php
@@ -1,13 +1,21 @@
 <?php
   $loadFooter = new loadFooter();
   $loadFooter->printStartFooter();
+  $loadFooter->printVisibleFooter();
   $loadFooter->printCustomPageFooter();
   $loadFooter->printEndFooter();
 
   class loadFooter {
+    public function printVisibleFooter() {
+      print("\n\t\t" . '<footer>' .
+            "\n\t\t\t" . '<p class="footer-message">' .
+            "\n\t\t\t\t" . 'Hello, this is a demo message for the footer. I am replacing this with a clear cache button for development.' .
+            "\n\t\t\t" . '</p>' .
+            "\n\t\t" . '</footer>');
+    }
+
     public function printStartFooter() {
       print("\n\t\t" . '</div>');
-      print("\n\t" . '</body>');
     }
 
     public function printCustomPageFooter() {
@@ -18,6 +26,7 @@
     }
 
     public function printEndFooter() {
+      print("\n\t" . '</body>');
       print("\n" . '</html>');
     }
   }
