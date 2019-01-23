28192e4ce04d99c16792880b7115c80aa319e92c
Alex Contento <github@senorcontento.com>
2019-01-23 00:46:16 -0500

    Created Basic XML version of Sitemap

--- sitemap/index.php
+++ sitemap/index.php
@@ -3,8 +3,8 @@
 $mainPage = new generateSitemap();
 
 $mainPage->setVars();
-$mainPage->generateSitemapTXTHeaders();
-$mainPage->generateSitemapTXT();
+$mainPage->generateSitemapXMLHeaders();
+$mainPage->generateSitemapXML();
 
 class generateSitemap {
   #public $exec_cat_path;
@@ -30,6 +30,38 @@ class generateSitemap {
       $this->exec_find_path = "/usr/local/bin/gfind";
       $this->exec_awk_path = "/usr/local/bin/gawk";
     }
+  }
+
+  public function generateSitemapXMLHeaders() {
+    header("Content-Type: text/xml");
+    header("Content-Type: application/xml");
+  }
+
+  public function generateSitemapXML() {
+    print('<?xml version="1.0" encoding="UTF-8"?>' . "\n" .
+          ' <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n");
+
+    $URLS = shell_exec($this->exec_find_path . ' ' .
+                       $_SERVER['DOCUMENT_ROOT'] . '/ | ' .
+                       $this->exec_grep_path . ' -v ".git" | ' . # Added the .git part for the development server
+                       $this->exec_grep_path . ' -v "/php_data" | ' . # and php_data for all servers
+                       $this->exec_grep_path . ' -v "/sql_admin" | ' .
+                       $this->exec_awk_path . " -F '" . $_SERVER['DOCUMENT_ROOT'] . "' '{ print \"<url><loc>\"$2\"</loc></url>\" }'"); # and sql_admin for all servers
+
+    $URLSArray = array_unique(explode("\n", $URLS));
+
+    #var_dump($URLSArray); # Dumps Raw Variable - Useful for Debugging
+
+    # Removes empty values from array
+    foreach($URLSArray as $key => $value)
+      if(empty($value))
+        unset($URLSArray[$key]);
+
+    foreach($URLSArray as $key => $value) {
+      print($value . "\n");
+    }
+
+    print(' </urlset>' . "\n");
   }
 
   public function generateSitemapTXTHeaders() {
