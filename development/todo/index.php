376a22c23884756c69da96b42d6f91be48a03534
Alex Contento <github@senorcontento.com>
2019-01-28 10:57:09 -0500

    Added Todo List

---
+++ development/todo/index.php
@@ -0,0 +1,65 @@
+<?php
+  function customPageHeader() {
+    print('<link rel="stylesheet" href="todo.css">');
+  }
+
+  $loadPage = new loadPage();
+  $mainPage = new mainPage();
+
+  $loadPage->loadHeader();
+  $mainPage->mainBody();
+  $loadPage->loadFooter();
+
+  class loadPage {
+    public function loadHeader() {
+      $PageTitle="TO-DO List";
+      include_once($_SERVER['DOCUMENT_ROOT'] . "/php_data/header.php");
+    }
+
+    public function loadFooter() {
+      include_once($_SERVER['DOCUMENT_ROOT'] . "/php_data/footer.php");
+    }
+  }
+
+  class mainPage {
+    public function mainBody() {
+      print('<form><ul class="todo">');
+
+      //<input type="checkbox" name="todo" disabled="disabled" checked>
+
+      print('<li class="todo"><input type="checkbox" name="todo" disabled="disabled"> ' .
+            'Make Site Work on Screen Readers' .
+            '</li>');
+
+      print('<li class="todo"><input type="checkbox" name="todo" disabled="disabled"> ' .
+            'Improve APC Interface' .
+            '</li>');
+
+      print('<li class="todo"><input type="checkbox" name="todo" disabled="disabled"> ' .
+            'Add API' .
+            '</li>');
+
+      print('<li class="todo"><input type="checkbox" name="todo" disabled="disabled"> ' .
+            'Finish Assignments' .
+            '</li>');
+
+      print('<li class="todo"><input type="checkbox" name="todo" disabled="disabled"> ' .
+            'Add Material Design Theme' .
+            '</li>');
+
+      print('<li class="todo"><input type="checkbox" name="todo" disabled="disabled"> ' .
+            'Finish Service Worker' .
+            '</li>');
+
+      print('<li class="todo"><input type="checkbox" name="todo" disabled="disabled"> ' .
+            'Add Support for Those That Are Dyslexic' .
+            '</li>');
+
+      print('<li class="todo"><input type="checkbox" name="todo" disabled="disabled"> ' .
+            'Improve Site Efficiency Using Chrome Developer Audits' .
+            '</li>');
+
+      print('</ul></form>');
+    }
+  }
+?>
