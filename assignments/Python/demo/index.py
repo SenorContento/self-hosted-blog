24f436d49a3d84658685c45c63d4bd63bb3cdf2a
Alex Contento <github@senorcontento.com>
2019-03-01 12:12:23 -0500

    I compiled Python3 module and added it to UWSGI. It works!

--- assignments/Python/demo/index.py
+++ assignments/Python/demo/index.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
-import sys
+#import sys
 
-def simple(): # Python 2
-    headers = ['content-type: text/html;charset=utf-8']
-    body = b'<h1>Hello World!</h1>\n'
-    print('\r\n'.join(headers + ['', '']).encode('utf-8'));
-    print(body);
+# Documentation Used to Help Add Python 3 Support
+# https://www.paulox.net/2017/04/04/how-to-use-uwsgi-with-python3-6-in-ubuntu/
+# https://www.digitalocean.com/community/tutorials/how-to-set-up-uwsgi-and-nginx-to-serve-python-apps-on-ubuntu-14-04
+# https://cuasan.wordpress.com/2015/01/25/simple-cgi-web-app-testing-setup-with-python-cgi-and-nginxuwsgi/
+# https://stackoverflow.com/questions/22798105/uwsgi-cannot-find-python-module
+# http://raspberrywebserver.com/cgiscripting/setting-up-nginx-and-uwsgi-for-cgi-scripting.html
+# https://www.paulox.net/2017/04/04/how-to-use-uwsgi-with-python3-6-in-ubuntu/
 
-def start(): # Python 3
-    headers = ['content-type: text/html;charset=utf-8']
-    body = b'<h1>Hello, world - Start()!</h1>\n' #this could be compressed, too, with the according information appended to headers
-    headers.append('content-length: {}'.format(len(body)))
-    sys.stdout.buffer.write('\r\n'.join(headers + ['', '']).encode('utf-8'))
-    sys.stdout.buffer.write(body)
+# Written By Alex
+
+def startup():
+    print("Custom Python 3 Web Interpreter Loaded");
 
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    return [b"UWSCGI Response"]
+    start_response('200 OK', [('Content-Type','text/html'), ('charset','utf-8')])
+    return [b"<h1>Hello World From Python 3!</h1>"]
 
-simple();
+startup();
