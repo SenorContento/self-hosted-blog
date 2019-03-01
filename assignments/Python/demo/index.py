329feca0ec84e99463d8a87f36825de8779a6bbb
Alex Contento <github@senorcontento.com>
2019-03-01 11:59:02 -0500

    UWSGI Seems to Use Python2, but it still doesn't output to browser

--- assignments/Python/demo/index.py
+++ assignments/Python/demo/index.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 import sys
 
-def start():
+def simple(): # Python 2
     headers = ['content-type: text/html;charset=utf-8']
-    body = b'<h1>Hello, world!</h1>\n' #this could be compressed, too, with the according information appended to headers
+    body = b'<h1>Hello World!</h1>\n'
+    print('\r\n'.join(headers + ['', '']).encode('utf-8'));
+    print(body);
+
+def start(): # Python 3
+    headers = ['content-type: text/html;charset=utf-8']
+    body = b'<h1>Hello, world - Start()!</h1>\n' #this could be compressed, too, with the according information appended to headers
     headers.append('content-length: {}'.format(len(body)))
     sys.stdout.buffer.write('\r\n'.join(headers + ['', '']).encode('utf-8'))
     sys.stdout.buffer.write(body)
@@ -12,4 +18,4 @@ def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
     return [b"UWSCGI Response"]
 
-start();
+simple();
