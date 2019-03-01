2baa3e2fbdd7f17f6de365b0c456ce56ff21e647
Alex Contento <github@senorcontento.com>
2019-03-01 11:20:21 -0500

    Pushing UWSGI Script

--- assignments/Python/demo/index.py
+++ assignments/Python/demo/index.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
-import sys
-headers = [ 'content-type: text/html;charset=utf-8' ]
-body = b'<h1>Hello, world!</h1>\n' #this could be compressed, too, with the according information appended to headers
-headers.append('content-length: {}'.format(len(body)))
-sys.stdout.buffer.write('\r\n'.join(headers + ['', '']).encode('utf-8'))
-sys.stdout.buffer.write(body)
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type','text/html')])
+    return [b"<h1>Hello World</h1>"]
