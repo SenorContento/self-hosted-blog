f09750db4f4a8fc346260dfe2538c4e6f7967fac
Alex Contento <github@senorcontento.com>
2019-03-02 13:29:05 -0500

    Added ability to read HTML from file

--- assignments/Python/index.py
+++ assignments/Python/index.py
@@ -1,5 +1,46 @@
+import os
+import subprocess
+
 from urllib import parse
 from ast import literal_eval
+
+def generatePage(env, title):
+    if(env['alex.server.type'] == "development"):
+        php_path = "/usr/local/bin/php";
+    else:
+        php_path = "/usr/bin/php";
+
+    os.environ["alex.server.name"] = env['alex.server.name'];
+    os.environ["alex.server.type"] = env['alex.server.type'];
+
+    # I need to add a way to automatically poll all variables and pass the ones beginning with php.alex
+    os.environ["alex.github.project"] = env['alex.github.project'];
+    os.environ["alex.github.branch"] = env['alex.github.branch'];
+    os.environ["alex.server.page.title"] = title;
+
+    header_path = env['DOCUMENT_ROOT'] + "/php_data/header.php";
+    proch = subprocess.Popen([php_path, header_path], stdout=subprocess.PIPE)
+    header = proch.stdout.read();
+
+    footer_path = env['DOCUMENT_ROOT'] + "/php_data/footer.php";
+    procf = subprocess.Popen([php_path, footer_path], stdout=subprocess.PIPE)
+    footer = procf.stdout.read();
+
+    return header, footer;
+
+def readFile(page):
+    with open(page, "r") as file:
+        lines = '';
+        for line in file:
+            lines = lines + line
+
+    return lines;
+
+def printForm(env, title):
+    header, footer = generatePage(env, title);
+
+    response = header.decode('utf-8') + readFile(env['DOCUMENT_ROOT'] + "/assignments/Python/form.html") + footer.decode('utf-8');
+    return response.encode('utf-8');
 
 def getRequest(env):
     # environ = dict(os.environ.items())
@@ -25,8 +66,7 @@ def init(env, start_response):
     query = getRequest(env);
     if len(query) == 0:
         start_response('200 OK', [('Content-Type','text/html'), ('charset','utf-8')])
-        # Insert Function to Print Form Here
-        return '<h1>Try Sending a POST or GET Request</h1>'.encode('utf-8');
+        return printForm(env, "Python - Form");
 
     start_response('200 OK', [('Content-Type','text/html'), ('charset','utf-8')])
 
