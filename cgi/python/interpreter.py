c6fbb47b6a46bb97f4c17edc771bb9b0ef842daa
Alex Contento <github@senorcontento.com>
2019-03-01 19:27:06 -0500

    I have yet to figure out how to read the environment variable. I also found out that invalid code breaks the server where I have to restart it.

--- cgi/python/interpreter.py
+++ cgi/python/interpreter.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-#import sys
-
 def startup():
     print("Custom Python 3 Web Interpreter Loaded");
+startup();
 
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html'), ('charset','utf-8')])
-    return [b"<h1>Hello World From Python 3!</h1>"]
 
-startup();
+    #response = b"<h1>Hello World From Python 3!</h1>";
+    response = '<h1>This is an encoded string!!!</h1>'.encode('utf-8') # Could also use ASCII
+
+    #readbytes = env['REQUEST_METHOD'].read() # returns bytes object
+    #readstr = readbytes.decode('utf-8')
+
+    return[response]
