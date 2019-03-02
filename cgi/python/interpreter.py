c32731d721d5e89d857add8ef0c8f8ef9282e2cc
Alex Contento <github@senorcontento.com>
2019-03-02 12:44:04 -0500

    Cleaned Up Code and Comments

--- cgi/python/interpreter.py
+++ cgi/python/interpreter.py
@@ -77,7 +77,6 @@ def grabPage(env, page_path):
     return proc.stdout.read();
 
 def printError404(env, start_response):
-    #global environment;
     start_response('404 File Not Found', [('Content-Type','text/html'), ('charset','utf-8')])
     return grabPage(env, "/errors/404/index.php");
 
@@ -107,31 +106,22 @@ def load(filepath, env, start_response):
     return function
 
 def application(env, start_response):
-    #global environment;
-    #environment = env;
-    #start_response('200 OK', [('Content-Type','text/html'), ('charset','utf-8')])
-
-    #response = b"<h1>Hello World From Python 3!</h1>";
-    #response = '<h1>This is an encoded string!!!</h1>'.encode('utf-8') # Could also use ASCII
-
     # https://uwsgi-docs.readthedocs.io/en/latest/Nginx.html#what-is-the-uwsgi-params-file
-    #testEnvironment = env['TESTME'].encode('utf-8')
-    path = env['PATH_INFO'].encode('utf-8')
-    root = env['DOCUMENT_ROOT'].encode('utf-8')
 
-    #QUERY_STRING - hello=yes
-    #REQUEST_METHOD - GET
-    #CONTENT_TYPE - ?
-    #CONTENT_LENGTH - ?
-    #REQUEST_URI - /path-to-python/script.py?hello=yes - Client?
-    #PATH_INFO - /path-to-python/script.py?hello=yes - Server?
-    #DOCUMENT_ROOT - /usr/local/Cellar/nginx/1.15.8/html
-    #SERVER_PROTOCOL - HTTP/2.0
-    #REMOTE_ADDR - 127.0.0.1 - Client?
-    #REMOTE_PORT - 49450 - Client?
-    #SERVER_ADDR - ?
-    #SERVER_PORT - 443
-    #SERVER_NAME - localhost
+    # Example Environment - https://localhost/path-to-python/script.py?hello=yes
+    # QUERY_STRING - hello=yes
+    # REQUEST_METHOD - GET
+    # CONTENT_TYPE - ?
+    # CONTENT_LENGTH - ?
+    # REQUEST_URI - /path-to-python/script.py?hello=yes - Client?
+    # PATH_INFO - /path-to-python/script.py?hello=yes - Server?
+    # DOCUMENT_ROOT - /usr/local/Cellar/nginx/1.15.8/html
+    # SERVER_PROTOCOL - HTTP/2.0
+    # REMOTE_ADDR - 127.0.0.1 - Client?
+    # REMOTE_PORT - 49450 - Client?
+    # SERVER_ADDR - ?
+    # SERVER_PORT - 443
+    # SERVER_NAME - localhost
 
-    file = root + path
-    return[load(file.decode('utf-8'), env, start_response)]
+    file = env['DOCUMENT_ROOT'] + env['PATH_INFO']
+    return[load(file, env, start_response)]
