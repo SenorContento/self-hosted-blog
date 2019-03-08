14e3d506d8d41654feffd093f23d55f964abc5ee
Alex Contento <github@senorcontento.com>
2019-03-07 23:43:16 -0500

    Trying Out Responsive Design for Chart

---
+++ assignments/Web_APIs/chart.js
@@ -0,0 +1,66 @@
+// https://developers.google.com/chart/interactive/docs/quick_start
+// https://jsfiddle.net/csabatoth/v3h9ycd4/2/
+// https://stackoverflow.com/a/22021224/6828099
+
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {'packages':['corechart']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChart);
+
+//width = (window.innerWidth/1.5);
+//height = (window.innerWidth/2.5);
+
+window.onresize = function(event) {
+  $(document).ready(function() {
+    drawChart();
+  });
+};
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
+
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Topping');
+  data.addColumn('number', 'Slices');
+  data.addRows([
+    ['Mushrooms', 3],
+    ['Onions', 1],
+    ['Olives', 1],
+    ['Zucchini', 1],
+    ['Pepperoni', 2]
+  ]);
+
+  // Set chart options
+  var options = {'title':'How Much Pizza I Ate Last Night',
+                 //'width': 100,
+                 //'height': 200,
+                 'colors': ['green', '#0f0f0f', 'red', 'blue', 'purple'],
+                 'is3D': true,
+                 'backgroundColor': {
+                   'fill': 'black',
+                   'fillOpacity': 1
+                 },
+                 fontName: 'OpenSans - Lighter',
+                 fontSize: '14',
+                 titleTextStyle: {
+                   color: '#90EE90',
+                   fontName: 'OpenSans - Lighter',
+                   fontSize: '22'
+                 },
+                 legend: {
+                   textStyle: {
+                     color: 'red',
+                     fontName: 'OpenSans - Lighter',
+                     fontSize: '14'
+                   }
+                 }
+               };
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
--- assignments/Web_APIs/index.php
+++ assignments/Web_APIs/index.php
@@ -1,6 +1,7 @@
 <?php
   function customPageHeader() {
     print("\n\t\t" . '<link rel="stylesheet" href="assignment11.css">');
+    print("\n\t\t" . '<script src="/js/jquery-3.3.1.min.js"></script>');
     print("\n\t\t" . '<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>');
   }
 
@@ -33,67 +34,9 @@
 
     public function drawTable() {
       print("
-      <script type=\"text/javascript\">
-
-      // Load the Visualization API and the corechart package.
-      google.charts.load('current', {'packages':['corechart']});
-
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.charts.setOnLoadCallback(drawChart);
-
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      function drawChart() {
-
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
-          ['Mushrooms', 3],
-          ['Onions', 1],
-          ['Olives', 1],
-          ['Zucchini', 1],
-          ['Pepperoni', 2]
-        ]);
-
-        // Set chart options
-        var options = {'title':'How Much Pizza I Ate Last Night',
-                       'width':600,
-                       'height':500,
-                       'colors': ['green', '#0f0f0f', 'red', 'blue', 'purple'],
-                       'is3D': true,
-                       'backgroundColor': {
-                         'fill': 'black',
-                         'fillOpacity': 1
-                       },
-                       fontName: 'OpenSans - Lighter',
-                       fontSize: '14',
-                       titleTextStyle: {
-                         color: '#90EE90',
-                         fontName: 'OpenSans - Lighter',
-                         fontSize: '22'
-                       },
-                       legend: {
-                         textStyle: {
-                           color: 'red',
-                           fontName: 'OpenSans - Lighter',
-                           fontSize: '14'
-                         }
-                       }
-                     };
-
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
-    </script>
-    <div id=\"chart_div\"></div>");
+      <script type=\"text/javascript\" src=\"chart.js\"></script>
+      <div id=\"chart_div\"></div>");
     }
-    // https://developers.google.com/chart/interactive/docs/quick_start
-    // https://jsfiddle.net/csabatoth/v3h9ycd4/2/
-    // https://stackoverflow.com/a/22021224/6828099
   }
 
   class loadPage {
