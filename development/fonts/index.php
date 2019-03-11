8fb77cb803d9ff73b5bc8d0fd3532b6e4f76ada6
Alex Contento <github@senorcontento.com>
2019-03-11 15:16:00 -0400

    Added Font Debug Page. There is something wrong with the way my fonts are chosen by the browser. I have to figure out the best way to fix it.

---
+++ development/fonts/index.php
@@ -0,0 +1,33 @@
+<?php
+  function customPageHeader() {
+    print("\n\t\t" . '<link rel="stylesheet" href="font-debug.css">');
+  }
+
+  $loadPage = new loadPage();
+  $mainPage = new mainPage();
+
+  $loadPage->loadHeader();
+  $mainPage->mainBody();
+  $loadPage->loadFooter();
+
+  class loadPage {
+    public function loadHeader() {
+      $PageTitle="CSCI 3000 - Web Development";
+      $root = isset($_SERVER['PWD']) ? $_SERVER['PWD'] : $_SERVER['DOCUMENT_ROOT'];
+      include_once($root . "/server_data/header.php");
+    }
+
+    public function loadFooter() {
+      $root = isset($_SERVER['PWD']) ? $_SERVER['PWD'] : $_SERVER['DOCUMENT_ROOT'];
+      include_once($root . "/server_data/footer.php");
+    }
+  }
+
+  class mainPage {
+    public function mainBody() {
+      print('<div style="display: inline-block; text-align: left;">');
+      print(file_get_contents("debug.html"));
+      print('</div>');
+    }
+  }
+?>
