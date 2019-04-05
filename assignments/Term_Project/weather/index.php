54aa102cbe2c9a1e95977c119cbc20277d377ef7
Alex Contento <github@senorcontento.com>
2019-04-05 01:09:11 -0400

    Added Parsed JSON Data

--- assignments/Term_Project/weather/index.php
+++ assignments/Term_Project/weather/index.php
@@ -26,7 +26,9 @@
   $mainPage->setAPIKey(getenv("alex.server.api.weatherstem"));
   $mainPage->setStations(array("dahlonega"));
 
-  $mainPage->parseJSON($mainPage->retrieveJSON());
+  $json = $mainPage->retrieveJSON();
+  $data = $mainPage->parseJSON($json);
+  $mainPage->printData($data);
 
   $loadPage->loadFooter();
 
@@ -59,11 +61,83 @@
       return $this->stations;
     }
 
+    public function printData($data) {
+      print("<image width='1000px' src='" . $data["dahlonega"]["camera"][0]["url"] . "'></img>");
+    }
+
     public function parseJSON($json) {
-      $response = json_decode($json);
+      $stations = json_decode($json);
+      $stations_array = [];
+
+      $station_count = 0;
+      foreach($stations as $station) {
+        $handle = $station->station->handle;
+
+        // Station
+        $stations_array[$handle]["station"][$station_count]["name"] = $station->station->name;
+        $stations_array[$handle]["station"][$station_count]["handle"] = $station->station->handle;
+        $stations_array[$handle]["station"][$station_count]["domain"]["name"] =$station->station->domain->name;
+        $stations_array[$handle]["station"][$station_count]["domain"]["handle"] = $station->station->domain->handle;
+
+        $stations_array[$handle]["station"][$station_count]["wunderground"] = $station->station->wunderground; // https://www.wunderground.com/weather/KGADAHLO40
+        $stations_array[$handle]["station"][$station_count]["twitter"] = $station->station->twitter; // https://twitter.com/UNGDWxSTEM
+        $stations_array[$handle]["station"][$station_count]["facebook"] = $station->station->facebook; // https://www.facebook.com/1881141655466992
+
+        $stations_array[$handle]["station"][$station_count]["latitude"] = $station->station->lat;
+        $stations_array[$handle]["station"][$station_count]["longitude"] = $station->station->lon;
+
+        // Record
+        $stations_array[$handle]["record"][$station_count]["now"] = $station->record->now;
+        $stations_array[$handle]["record"][$station_count]["derived"] = $station->record->derived;
+        $stations_array[$handle]["record"][$station_count]["id"] = $station->record->id;
+
+        $stations_array[$handle]["record"][$station_count]["raintime"] = $station->record->last_rain_time;
+        $stations_array[$handle]["record"][$station_count]["time"] = $station->record->time;
+
+        $stations_array[$handle]["record"][$station_count]["hilo"]["property"] = $station->record->hilo->property;
+        $stations_array[$handle]["record"][$station_count]["hilo"]["name"] = $station->record->hilo->name;
+        $stations_array[$handle]["record"][$station_count]["hilo"]["type"] = $station->record->hilo->type;
+
+        $stations_array[$handle]["record"][$station_count]["hilo"]["temp"]["max"] = $station->record->hilo->max;
+        $stations_array[$handle]["record"][$station_count]["hilo"]["temp"]["min"] = $station->record->hilo->min;
+
+        $stations_array[$handle]["record"][$station_count]["hilo"]["time"]["max"] = $station->record->hilo->max_time;
+        $stations_array[$handle]["record"][$station_count]["hilo"]["time"]["min"] = $station->record->hilo->min_time;
+
+        $stations_array[$handle]["record"][$station_count]["hilo"]["unit"] = $station->record->hilo->unit;
+        $stations_array[$handle]["record"][$station_count]["hilo"]["symbol"] = $station->record->hilo->symbol;
+
+        $camera_count = 0;
+        foreach($station->station->cameras as $camera) {
+          $stations_array[$handle]["camera"][$camera_count]["name"] = $camera->name;
+          $stations_array[$handle]["camera"][$camera_count]["url"] = $camera->image;
+          $camera_count = $camera_count + 1;
+        }
+
+        $readings_count = 0;
+        foreach($station->record->readings as $reading) {
+          $stations_array[$handle]["reading"][$readings_count]["symbol"] = $reading->unit_symbol;
+          $stations_array[$handle]["reading"][$readings_count]["type"] = $reading->sensor_type;
+          $stations_array[$handle]["reading"][$readings_count]["id"] = $reading->id;
+          $stations_array[$handle]["reading"][$readings_count]["sensor"] = $reading->sensor;
+          $stations_array[$handle]["reading"][$readings_count]["value"] = $reading->value;
+          $stations_array[$handle]["reading"][$readings_count]["transmitter"] = $reading->transmitter;
+          $stations_array[$handle]["reading"][$readings_count]["unit"] = $reading->unit;
+          $readings_count = $readings_count + 1;
+        }
+
+        $station_count = $station_count + 1;
+      }
+
+      //print_r($stations_array);
+      return $stations_array;
+    }
+
+    public function debugJSON($json) {
+      $stations = json_decode($json);
       //print_r($response);
 
-      foreach($response as $station) {
+      foreach($stations as $station) {
         // Read stdClass Object
         // https://stackoverflow.com/a/8510937/6828099
 
