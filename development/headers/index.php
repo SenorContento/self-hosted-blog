371556b0ea81d975e44aea0c5af7a70a42543d20
Alex Contento <github@senorcontento.com>
2019-04-08 13:14:05 -0400

    Header Dump For Debugging

---
+++ development/headers/index.php
@@ -0,0 +1,67 @@
+<?php
+  function customPageHeader() {
+    //print("\n\t\t" . '<link rel="stylesheet" href="template.css">');
+    //print("\n\t\t" . '<script src="template.js"></script>');
+  }
+
+  function customPageFooter() {
+    print("\n\t\t" . '<link rel="stylesheet" href="headers.css">');
+    //print("\n\t\t" . '<script src="delayed.js"></script>');
+  }
+
+  function customMetadata() {
+    print("\n\t\t" . '<!--Custom Metadata-->');
+    print("\n\t\t" . '<meta name="description" content="Dump Headers!!!">');
+    print("\n\t\t" . '<meta name="keywords" content="Headers,PHP,NGinx">');
+    print("\n\t\t" . '<meta name="robots" content="noindex, nofollow">');
+    print("\n\t\t" . '<!--End Custom Metadata-->');
+  }
+
+  $loadPage = new loadPage();
+  $mainPage = new mainPage();
+
+  $loadPage->loadHeader();
+  //$mainPage->printSourceCodeLink();
+  $mainPage->printHeaders();
+  $loadPage->loadFooter();
+
+  class mainPage {
+    public function printSourceCodeLink() {
+      print('<a class="source-code-link" href="' . getenv('alex.github.project') . '/tree/' . getenv('alex.github.branch') . $_SERVER['SCRIPT_NAME'] . '">View Source Code</a><br>');
+      //print('<a class="source-code-link" href="' . getenv('alex.github.project') . '/tree/' . getenv('alex.github.branch') . "/assignments/Term_Project" . $_SERVER['SCRIPT_NAME'] . '">View Source Code</a><br>'); // For Term Project
+    }
+
+    public function printHeaders() {
+      print("<div class='center'>");
+      print("<p class='headers'>");
+      foreach ($this->getallheaders() as $name => $value) {
+          print("$name: $value<br>");
+      }
+      print("</p></div>");
+    }
+
+    // https://www.php.net/manual/en/function.getallheaders.php#84262
+    function getallheaders() {
+      $headers = [];
+      foreach ($_SERVER as $name => $value) {
+        if (substr($name, 0, 5) == 'HTTP_') {
+          $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+        }
+      }
+      return $headers;
+    }
+  }
+
+  class loadPage {
+    public function loadHeader() {
+      $PageTitle="Template PHP Page!!!";
+      $root = isset($_SERVER['PWD']) ? $_SERVER['PWD'] : $_SERVER['DOCUMENT_ROOT'];
+      include_once($root . "/server-data/header.php");
+    }
+
+    public function loadFooter() {
+      $root = isset($_SERVER['PWD']) ? $_SERVER['PWD'] : $_SERVER['DOCUMENT_ROOT'];
+      include_once($root . "/server-data/footer.php");
+    }
+  }
+?>
